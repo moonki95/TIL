7933738a36008cce762b024f49d8e7e1d246d0a9
Moonki Jang <mkjang0905@naver.com>
2021-12-10 15:19:00 +0900

    Kotlin Project study

---
+++ Kotlin_study/app/src/main/java/com/e/kotlin_study/activity/HelloActivity.kt
@@ -0,0 +1,19 @@
+package com.e.kotlin_study.activity
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.e.kotlin_study.R
+import kotlinx.android.synthetic.main.activity_hello.*
+
+class HelloActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+
+        btnHello.setOnClickListener(){
+            Toast.makeText(this, "토스트미시지",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
