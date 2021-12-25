e1f4c3e423b6df90ea9249207bb56f2816aef70c
Moonki Jang <mkjang0905@naver.com>
2021-12-25 21:38:24 +0900

    21.12.25

--- Kot_study/app/src/main/java/com/autoever/kot_study/MainActivity.kt
+++ Kot_study/app/src/main/java/com/autoever/kot_study/MainActivity.kt
@@ -2,14 +2,38 @@ package com.autoever.kot_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.autoever.kot_study.databinding.ActivityMainBinding
+import android.util.Log
+import android.widget.CompoundButton
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    val listener by lazy {
+        CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                when (buttonView.id) {
+                    R.id.checkBox4 -> Log.d("CheckBox", "1번선택")
+                    R.id.checkBox5 -> Log.d("CheckBox", "2번선택")
+                    R.id.checkBox6 -> Log.d("CheckBox", "3번선택")
+                }
+            } else {
+                when (buttonView.id) {
+                    R.id.checkBox4 -> Log.d("CheckBox", "1번해제")
+                    R.id.checkBox5 -> Log.d("CheckBox", "2번해제")
+                    R.id.checkBox6 -> Log.d("CheckBox", "3번해제")
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.checkBox4.setOnCheckedChangeListener(listener)
+        binding.checkBox5.setOnCheckedChangeListener(listener)
+        binding.checkBox6.setOnCheckedChangeListener(listener)
 
     }
 }
